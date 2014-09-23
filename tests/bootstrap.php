331bf86f7370b7b3650561068055cdd7494de079
Jon Cram <jon@webignition.net>
2014-09-23 18:26:49 +0100

    Update deps, phpunit version and travis test matrix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,5 @@
 <?php
 ini_set('display_errors', 'On');
-require_once  __DIR__ . '/../vendor/autoload.php';
+ini_set('error_reporting', -1);
 
-function autoload( $rootDir ) {
-    spl_autoload_register(function( $className ) use ( $rootDir ) {        
-        $file = sprintf(
-            '%s/%s.php',
-            $rootDir,
-            str_replace( '\\', '/', $className )
-        );        
-        
-        if ( file_exists($file) ) {
-            require $file;
-        }
-    });
-}
-
-autoload( '/usr/share/php' );
-autoload( __DIR__ . '/');
-autoload( __DIR__ . '/DirectiveFactory');
-autoload( __DIR__ . '/Parser');
+require __DIR__ . '/../vendor/autoload.php';
